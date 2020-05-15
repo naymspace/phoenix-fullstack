2fe40ba734057ab9896e142dd25bc1a09358e22b
Kilian Gärtner <kilian.gaertner@naymspace.de>
2020-05-15 17:56:50 +0200

    Replace prettier eslint with airbnb

--- templates/vue/assets/.eslintrc.js
+++ templates/vue/assets/.eslintrc.js
@@ -7,15 +7,15 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/airbnb"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "comma-dangle": "off",
   },
   overrides: [
     {
