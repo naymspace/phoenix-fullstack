7922436525c15e97448a46bd99f7489a4e3fd69f
Kilian Gärtner <kilian.gaertner@naymspace.de>
2020-05-16 16:05:43 +0200

    Release 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixTemplate.MixProject do
   def project do
     [
       app: :phoenix_template,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
